541ff3bded96482ce9fe0a3d19744d462bd3a606
Jake Welton <jwelton122@googlemail.com>
2016-06-24 20:56:33 +0100

    Add basic category request logic

--- BreweryDB/CategoryRequest.swift
+++ BreweryDB/CategoryRequest.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+public class CategoryRequest {
+    private let requestBuilder = RequestBuilder(endPoint: .Categories)
+    private var request: NSURLRequest
+    
+    public var requestURL: NSURLRequest {
+        return request
+    }
+    
+    public init?() {
+        guard let url = requestBuilder.buildRequest([BeerRequestParam: String]()) else {
+            return nil
+        }
+        
+        request = url
+    }
+    
+    public func loadCategoriesWithCompletionHandler(completionHandler: ((categories: [Category]?)->Void)) {
+        NSURLSession.sharedSession().dataTaskWithRequest(requestURL) { data, response, error in
+            guard let returnedData = data,
+                let response = response as? NSHTTPURLResponse where response.statusCode == 200 else {
+                    completionHandler(categories: nil)
+                    return
+            }
+            
+            let jsonParser = JSONParser<Category>(rawData: returnedData)
+            jsonParser?.extractObjectsWithCompletionHandler(completionHandler)
+            
+            }.resume()
+    }
+}
+
+extension CategoryRequest: CustomStringConvertible {
+    public var description: String {
+        return requestURL.URL?.absoluteString ?? ""
+    }
+}
